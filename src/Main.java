f9915063aa5e53026a13303275c314b691a36c34
Bh4n <Bh4n@users.noreply.github.com>
2021-05-01 04:17:04 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
         Student student1 = new Student(1, "Enes Bahan", "Morova", "bh**@gmail.com", 537, 21);
         StudentManager studentManager = new StudentManager();
+
         studentManager.add(student1);
         studentManager.studentNumber(student1);
         System.out.println("--------------------------");
